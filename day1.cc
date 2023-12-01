2e5d2c852c294c569c9b58d6a0e567b668d31d10
Aaron Brady <aaron.brady@shopify.com>
2023-12-01 11:55:27 -0500

    day1, part1

--- day1.cc
+++ day1.cc
@@ -9,8 +9,24 @@ using namespace std;
 int main() {
   std::ifstream ifs("input1");
   std::string line;
+  int runningTotal = 0;
 
   while (ifs.good()) {
     getline(ifs, line);
+    int firstNumber = -1;
+    int lastNumber = -1;
+    for (char c : line) {
+      if(c >= '0' && c <= '9') {
+        if(firstNumber == -1) {
+          firstNumber = c - '0';
+        }
+        lastNumber = c - '0';
+      }
+    }
+    if(firstNumber != -1) {
+      cout << firstNumber << lastNumber << endl;
+      runningTotal += (firstNumber * 10) + lastNumber;
+    }
   }
+  cout << runningTotal << endl;
 }
